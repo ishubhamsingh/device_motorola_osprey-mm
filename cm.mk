174aa26c3b0ea97cca43a8d0ffd575bbcb3bc4bb
ishubhamsingh <coolsks94@gmail.com>
2016-05-27 12:12:21 +0000

    Beanstalk bringup
    Change-Id: I459e808b0f928b7d7927bb578cd0823e1f5f0654

--- cm.mk
+++ cm.mk
@@ -20,7 +20,11 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := cm_osprey
+PRODUCT_NAME := bs_osprey
 PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G (3rd gen)
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto G (3rd gen)"
